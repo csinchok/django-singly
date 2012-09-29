5a00cd6d0f5f51a515b0ad0eb2a731bb5a3bf823
Chris Sinchok <csinchok@theonion.com>
2012-09-29 14:28:15 -0500

    I can't spell

--- singly/models.py
+++ singly/models.py
@@ -6,5 +6,5 @@ from django.contrib.auth.models import User
 class SinglyProfile(models.Model):
     user = models.OneToOneField(User, related_name='singly')
     access_token = models.CharField(max_length=255)
-    account = modles.CharField(max_length=255)
+    account = models.CharField(max_length=255)
     
