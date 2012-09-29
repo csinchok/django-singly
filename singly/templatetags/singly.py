eeafe749acda0a2ab6fb44d2a2028dc867ece19b
Chris Sinchok <csinchok@theonion.com>
2012-09-29 14:30:51 -0500

    getattr, bro

--- singly/templatetags/singly.py
+++ singly/templatetags/singly.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def singly_login_url(context, service):
-    redirect_uri = settings.get('SINGLY_CALLBACK_URL', None)
+    redirect_uri = settings.getattr('SINGLY_CALLBACK_URL', None)
     if redirect_uri is None:
         redirect_uri = "%s/%s" % (context['request'].get_host(), reverse('singly:singly_callback'))
     auth_url = "https://api.singly.com/oauth/authorize?client_id=%s&redirect_uri=%s&service=%s" % (
