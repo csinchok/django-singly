2aa7f4b30807dc2205c727abbe4885318bd283c4
Chris Sinchok <csinchok@theonion.com>
2012-09-29 19:38:25 -0500

    Debugging

--- singly/backends.py
+++ singly/backends.py
@@ -16,42 +16,41 @@ class SinglyBackend(object):
             return None
 
     def authenticate(self, code=None):
-        print('authing')
-        try:
-            params = {
-                'code': code,
-                'client_id': settings.SINGLY_CLIENT_ID,
-                'client_secret': settings.SINGLY_CLIENT_SECRET
-            }
-            response = requests.post('https://api.singly.com/oauth/access_token', data=params)
-            if response.status_code == 200:
-                access_token = response.json.get('access_token')
-                account = response.json.get('account')
-                try:
-                    user = SinglyProfile.objects.get(account=account).user
-                    user.access_token = access_token
-                    user.save()
-                    return user
-                except SinglyProfile.DoesNotExist:
-                    profile_response = requests.get("https://api.singly.com/v0/profile", params={'access_token': access_token})
-                    user_kwargs = {'username': account}
-                    if profile_response.status_code == 200:
-                        if 'email' in profile_response.json:
-                            user_kwargs['username'] = profile_response.json['email']
-                        elif 'name' in profile_response.json:
-                            user_kwargs['username'] = profile_response.json['name'].lower().replace(' ', '')
-                        if profile_response.json.get('name'):
-                            try:
-                                user_kwargs['first_name'], user_kwargs['last_name'] = profile_response.json['name'].split(' ')
-                            except IndexError:
-                                pass
-                        user_kwargs['email'] = profile_response.json.get('email')
-                    user = User(**user_kwargs)
-                    user.save()
-                    SinglyProfile.objects.create(user=user, account=account, access_token=access_token)
-                    return user
-            else:
-                print("[%s] %s" % (response.status_code, response.text))
-        except Exception as e:
-            print(e)
+        params = {
+            'code': code,
+            'client_id': settings.SINGLY_CLIENT_ID,
+            'client_secret': settings.SINGLY_CLIENT_SECRET
+        }
+        response = requests.post('https://api.singly.com/oauth/access_token', data=params)
+        if response.status_code == 200:
+            access_token = response.json.get('access_token')
+            account = response.json.get('account')
+            try:
+                user = SinglyProfile.objects.get(account=account).user
+                user.access_token = access_token
+                user.save()
+                return user
+            except SinglyProfile.DoesNotExist:
+                print("Creating new user...")
+                profile_response = requests.get("https://api.singly.com/v0/profile", params={'access_token': access_token})
+                user_kwargs = {'username': account}
+                if profile_response.status_code == 200:
+                    if 'email' in profile_response.json:
+                        user_kwargs['username'] = profile_response.json['email']
+                    elif 'name' in profile_response.json:
+                        user_kwargs['username'] = profile_response.json['name'].lower().replace(' ', '')
+                    if profile_response.json.get('name'):
+                        try:
+                            user_kwargs['first_name'], user_kwargs['last_name'] = profile_response.json['name'].split(' ')
+                        except IndexError:
+                            pass
+                    user_kwargs['email'] = profile_response.json.get('email')
+                else:
+                    print(profile_response.text)
+                user = User(**user_kwargs)
+                user.save()
+                SinglyProfile.objects.create(user=user, account=account, access_token=access_token)
+                return user
+        else:
+            print("[%s] %s" % (response.status_code, response.text))
         return None
